674e797abd5bc35f781ecfb9e04c4875d08f0f93
PawWitGit <git.pawelwitala@gmail.com>
2022-02-16 23:02:04 +0100

    imporove gui

--- masters_project/main.py
+++ masters_project/main.py
@@ -1,8 +1,9 @@
 from cProfile import label
+from calendar import calendar
 from msilib.schema import LaunchCondition
 import sys
 from turtle import right
-from PySide6.QtCore import QRect,Qt
+from PySide6.QtCore import QRect, Qt, QTimer
 from PySide6.QtWidgets import (
     QApplication,
     QLabel,
@@ -13,51 +14,66 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QListWidgetItem,
     QTextEdit,
-    QLineEdit
-
-    
-    
+    QLineEdit,
+    QCalendarWidget,
 )
 
 from datetime import datetime
 
 from database.db_connect import DbConnection
 
+
 class MainWindow(QLabel):
     def __init__(self, parent=None):
-        
-        self.db_ip_adress = '192.168.55.115' 
-        
+
+        self.db_ip_adress = "192.168.55.115"
+
         super(MainWindow, self).__init__()
-        
+
         db_connection = DbConnection()
 
-        MainWindow.setGeometry(self,300,300,700,520)
+        MainWindow.setGeometry(self, 300, 300, 700, 700)
 
-        item_list = ['PM1','PM2.5','PM10','Temperatura','Ciśnienie', 'Wilgotność']
+        item_list = ["PM1", "PM2.5", "PM10", "Temperatura", "Ciśnienie", "Wilgotność"]
         menu_widget = QListWidget()
         for i in range(len(item_list)):
             item = QListWidgetItem(f"{item_list[i]}")
-            menu_widget.addItem(item)      
-             
+            menu_widget.addItem(item)
+
         label_1 = QLabel("Db connect", self)
-        label_1.setGeometry(250,60, 150 ,100)
+        label_1.setGeometry(250, 60, 150, 100)
         if db_connection.check_db_connect() == True:
             label_1.setText("Połączono\nz bazą danych")
-            label_1.setStyleSheet('color: blue')
+            label_1.setStyleSheet("color: blue")
         else:
             label_1.setText("Brak połączenia\nz bazą danych")
-            label_1.setStyleSheet('color: red')
-            
-        edit_label_1 = QLineEdit(f'Adres IP bazy danych {self.db_ip_adress}', self)
-        edit_label_1.setGeometry(10, 2, 160,20)
-        
-        menu_widget.setGeometry(QRect(30,30,200,21))
-        
-        button_1 = QPushButton('Odśwież połaczenie\n z bazą danych', self)
-        button_1.clicked.connect(lambda: print(datetime.now(),"Refresh Db conn", edit_label_1.text()))
-        button_1.setGeometry(10,60,230,100)    
-        
+            label_1.setStyleSheet("color: red")
+
+        label_2 = QLabel("DB IP ADRESS:", self)
+        label_2.setGeometry(10, 2, 160, 20)
+
+        edit_label_1 = QLineEdit(f"{self.db_ip_adress}", self)
+        edit_label_1.setGeometry(10, 22, 160, 20)
+
+        calendar_1 = QCalendarWidget(self)
+        calendar_1.setGridVisible(True)
+        calendar_1.setGeometry(10, 200, 200, 200)
+
+        calendar_2 = QCalendarWidget(self)
+        calendar_2.setGridVisible(True)
+        calendar_2.setGeometry(220, 200, 200, 200)
+
+        button_1 = QPushButton("Odśwież połaczenie\n z bazą danych", self)
+        button_1.clicked.connect(
+            lambda: print(
+                datetime.now(),
+                "Refresh Db conn",
+                edit_label_1.text(),
+                "\n",
+                calendar_1.selectedDate(),
+            )
+        )
+        button_1.setGeometry(10, 60, 230, 100)
 
 
 if __name__ == "__main__":
